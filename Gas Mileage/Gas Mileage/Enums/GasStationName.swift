8abad95ea8c0357f0150f5563490211ddaa2e97e
Nick Shuklin <nickshuklin@icloud.com>
2024-06-11 12:52:39 +0200

    2 new files added

---
+++ Gas Mileage/Gas Mileage/Enums/GasStationName.swift
@@ -0,0 +1,18 @@
+//
+//  GasStationName.swift
+//  Gas Mileage
+//
+//  Created by Nick Shuklin on 6/6/24.
+//
+
+import Foundation
+
+enum GasStationName: String {
+	case shell = "Shell"
+	case chevron = "Chevron"
+	case lukoil = "Lukoil"
+	case gazprom = "Gazprom"
+	case mobil = "Mobil"
+	case repsol = "Repsol"
+	case valero = "Valero"
+}
