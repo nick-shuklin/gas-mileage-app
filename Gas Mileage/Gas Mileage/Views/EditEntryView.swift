56786f1d18dfdacd7e20b65b0fac17b26327f97d
Nick Shuklin <nickshuklin@icloud.com>
2024-06-12 20:34:34 +0200

    Edit entry view updated. Still WIP

--- Gas Mileage/Gas Mileage/Views/EditEntryView.swift
+++ Gas Mileage/Gas Mileage/Views/EditEntryView.swift
@@ -2,17 +2,77 @@
 //  EditEntryView.swift
 //  Gas Mileage
 //
-//  Created by Nikolay Shuklin on 6/12/24.
+//  Created by Nick Shuklin on 6/12/24.
 //
 
 import SwiftUI
 
 struct EditEntryView: View {
+	@Bindable var item: GasFillEntry
+	@Environment(\.dismiss) private var dismiss
+	@Environment(\.modelContext) private var modelContext
+	static var nf = NumberFormatter()
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		Form {
+			Section {
+				DatePicker("Date", selection: $item.timestamp)
+				// TODO: add all station name cases
+				Picker("Gas Station", selection: $item.gasStationName) {
+					Text("Shell")
+						.tag(GasStationName.shell)
+					Text("Chevron")
+						.tag(GasStationName.chevron)
+					Text("Mobil")
+						.tag(GasStationName.mobil)
+				}
+				.pickerStyle(.menu)
+			}
+			Section {
+				HStack {
+					Text("Odometer")
+					Spacer()
+					TextField(
+						"Odometer",
+						value: $item.odometer,
+						formatter: EditEntryView.nf,
+						onEditingChanged: { _ in
+							
+						}, onCommit: {
+							
+						})
+					.keyboardType(.numberPad)
+				}
+				HStack {
+					Text("Total")
+					Spacer()
+					TextField(
+						"Total",
+						value: $item.total,
+						formatter: EditEntryView.nf,
+						onEditingChanged: { _ in
+							
+						}, onCommit: {
+							
+						})
+					.keyboardType(.numberPad)
+				}
+			}
+			Section {
+				Toggle(String("Tank filled up?"), isOn: $item.isFilledUp)
+				Toggle(String("Paid cash?"), isOn: $item.isPaidCash)
+			}
+		}
+		HStack {
+			Button("Cancel") {
+				dismiss()
+			}
+			Spacer()
+			Button("Confirm") {
+				modelContext.insert(item)
+				dismiss()
+			}
+		}
+		.padding(20)
     }
-}
-
-#Preview {
-    EditEntryView()
 }
