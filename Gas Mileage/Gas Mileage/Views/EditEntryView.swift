05fa110fbe82f1f703916a1551a9f850ae435b6b
Nick Shuklin <nickshuklin@icloud.com>
2024-06-12 15:43:18 +0200

    New view added

---
+++ Gas Mileage/Gas Mileage/Views/EditEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  EditEntryView.swift
+//  Gas Mileage
+//
+//  Created by Nikolay Shuklin on 6/12/24.
+//
+
+import SwiftUI
+
+struct EditEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditEntryView()
+}
