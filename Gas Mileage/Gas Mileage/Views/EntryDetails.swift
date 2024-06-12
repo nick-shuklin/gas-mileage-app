1754af2183ce1ee118560c41a77a5b35a04a5a3c
Nick Shuklin <nickshuklin@icloud.com>
2024-06-12 20:33:28 +0200

    WIP entry details view

--- Gas Mileage/Gas Mileage/Views/EntryDetails.swift
+++ Gas Mileage/Gas Mileage/Views/EntryDetails.swift
@@ -2,26 +2,22 @@
 //  EntryDetails.swift
 //  Gas Mileage
 //
-//  Created by Nikolay Shuklin on 6/11/24.
+//  Created by Nick Shuklin on 6/11/24.
 //
 
 import SwiftUI
 import SwiftData
 
 struct EntryDetails: View {
-	@Environment(\.modelContext) private var modelContext
-	@Query private var items: [GasFillEntry]
-	var item: GasFillEntry
+	@Bindable var item: GasFillEntry
 	
 	var body: some View {
 		VStack {
-			Text(item.gasStationName.rawValue)
-			Text(item.timestamp, format: Date.FormatStyle(date: .abbreviated, time: .shortened))
-			Text(item.odometerReadingDescription)
-			Text(item.gasPriceDescription)
-			Text(item.totalAmountDescription)
-			Text(item.gasMileageDescription)
-			Text(item.volumeDescription)
+			HStack {
+				Text("Odometer")
+				Spacer()
+				Text("\(item.odometer)")
+			}
 		}
 	}
 }
