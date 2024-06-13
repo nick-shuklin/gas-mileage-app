c83b1e5b6423cfda1aec88f415f302358bfe680c
Nick Shuklin <nickshuklin@icloud.com>
2024-06-13 11:04:25 +0200

    Updated edit entry view screen

--- Gas Mileage/Gas Mileage/Views/EditEntryView.swift
+++ Gas Mileage/Gas Mileage/Views/EditEntryView.swift
@@ -11,51 +11,80 @@ struct EditEntryView: View {
 	@Bindable var item: GasFillEntry
 	@Environment(\.dismiss) private var dismiss
 	@Environment(\.modelContext) private var modelContext
+	// FIXME: formatter should be specified separately for each value
 	static var nf = NumberFormatter()
 	
     var body: some View {
 		Form {
 			Section {
 				DatePicker("Date", selection: $item.timestamp)
-				// TODO: add all station name cases
 				Picker("Gas Station", selection: $item.gasStationName) {
-					Text("Shell")
-						.tag(GasStationName.shell)
-					Text("Chevron")
-						.tag(GasStationName.chevron)
-					Text("Mobil")
-						.tag(GasStationName.mobil)
+					ForEach(GasStationName.allCases) { name in
+						Text(name.rawValue)
+							.tag(name)
+					}
 				}
 				.pickerStyle(.menu)
 			}
 			Section {
-				HStack {
-					Text("Odometer")
-					Spacer()
-					TextField(
-						"Odometer",
-						value: $item.odometer,
-						formatter: EditEntryView.nf,
-						onEditingChanged: { _ in
-							
-						}, onCommit: {
-							
+				VStack {
+					HStack {
+						Text("Odometer")
+						Spacer()
+						TextField(
+							"Odometer",
+							value: $item.odometer,
+							formatter: EditEntryView.nf,
+							onEditingChanged: { _ in
+								
+							}, onCommit: {
+								
 						})
-					.keyboardType(.numberPad)
-				}
-				HStack {
-					Text("Total")
-					Spacer()
-					TextField(
-						"Total",
-						value: $item.total,
-						formatter: EditEntryView.nf,
-						onEditingChanged: { _ in
-							
-						}, onCommit: {
-							
+						.keyboardType(.numberPad)
+					}
+					HStack {
+						Text("Total")
+						Spacer()
+						TextField(
+							"Total",
+							value: $item.total,
+							formatter: EditEntryView.nf,
+							onEditingChanged: { _ in
+								
+							}, onCommit: {
+								
 						})
-					.keyboardType(.numberPad)
+						.keyboardType(.decimalPad)
+						.border(.background)
+					}
+					HStack {
+						Text("Price")
+						Spacer()
+						TextField(
+							"Price",
+							value: $item.gasPrice,
+							formatter: EditEntryView.nf,
+							onEditingChanged: { _ in
+								
+							}, onCommit: {
+								
+						})
+						.keyboardType(.decimalPad)
+					}
+					HStack {
+						Text("Volume")
+						Spacer()
+						TextField(
+							"Volume",
+							value: $item.volume,
+							formatter: EditEntryView.nf,
+							onEditingChanged: { _ in
+								
+							}, onCommit: {
+								
+						})
+						.keyboardType(.decimalPad)
+					}
 				}
 			}
 			Section {
