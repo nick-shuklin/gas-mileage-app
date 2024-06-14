c4a61c2d7a7731d72a5208948bf2589adbb8c322
Nick Shuklin <nickshuklin@icloud.com>
2024-06-14 11:25:22 +0200

    Added new settings view

---
+++ Gas Mileage/Gas Mileage/Views/SettingsView.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsView.swift
+//  Gas Mileage
+//
+//  Created by Nikolay Shuklin on 6/14/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+		Section("About") {
+			Text("Version 1.0.0")
+		}
+    }
+}
