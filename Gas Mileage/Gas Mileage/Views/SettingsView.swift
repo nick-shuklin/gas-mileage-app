3bbf16615125ec38a0652e4ef9cc67edcfc261b5
Nick Shuklin <nickshuklin@icloud.com>
2024-06-14 11:47:13 +0200

    Settings view updated with app version

--- Gas Mileage/Gas Mileage/Views/SettingsView.swift
+++ Gas Mileage/Gas Mileage/Views/SettingsView.swift
@@ -2,15 +2,17 @@
 //  SettingsView.swift
 //  Gas Mileage
 //
-//  Created by Nikolay Shuklin on 6/14/24.
+//  Created by Nick Shuklin on 6/14/24.
 //
 
 import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-		Section("About") {
-			Text("Version 1.0.0")
+		Form {
+			Section("About") {
+				Text("Version \(AppVersionProvider.appVersion())")
+			}
 		}
     }
 }
