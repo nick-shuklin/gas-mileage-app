8ed2a541d26a3bc73031d3d03f5349c4b115b6a9
Nick Shuklin <nickshuklin@icloud.com>
2024-06-21 18:28:14 +0200

    New view now has elements to display

--- Gas Mileage/Gas Mileage/Views/MainView.swift
+++ Gas Mileage/Gas Mileage/Views/MainView.swift
@@ -2,17 +2,42 @@
 //  MainView.swift
 //  Gas Mileage
 //
-//  Created by Nikolay Shuklin on 6/21/24.
+//  Created by Nick Shuklin on 6/21/24.
 //
 
 import SwiftUI
+import SwiftData
 
 struct MainView: View {
+	static let amountOfEntriesToDisplay = 10
+	static var fetchDescriptor: FetchDescriptor<GasFillEntry> {
+		var descriptor = FetchDescriptor<GasFillEntry>(
+			predicate: #Predicate { $0.isPaidCash == true },
+			sortBy: [
+				.init(\.timestamp)
+			]
+		)
+		descriptor.fetchLimit = amountOfEntriesToDisplay
+		return descriptor
+	}
+
+	@Query(MainView.fetchDescriptor) private var items: [GasFillEntry]
+	@Environment(\.modelContext) private var modelContext
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		HStack {
+			Spacer()
+			NavigationSplitView {
+				List(items) { EntryRowView(item: $0) }
+					.navigationTitle("Last \(MainView.amountOfEntriesToDisplay) of entries")
+			} detail: {
+				Text("Select an item")
+			}
+		}
     }
 }
 
 #Preview {
     MainView()
+		.modelContainer(for: GasFillEntry.self, inMemory: false)
 }
