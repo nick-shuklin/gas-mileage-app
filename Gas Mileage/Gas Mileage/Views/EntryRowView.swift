43ecb351dd21fc5773007870a5fbaae768bd9b6b
Nick Shuklin <nickshuklin@icloud.com>
2024-06-21 16:53:54 +0200

    Entry row view WIP

--- Gas Mileage/Gas Mileage/Views/EntryRowView.swift
+++ Gas Mileage/Gas Mileage/Views/EntryRowView.swift
@@ -9,23 +9,38 @@ import SwiftUI
 
 struct EntryRowView: View {
 	var item: GasFillEntry
+	@Environment(\.locale) var locale: Locale
 	
     var body: some View {
-		HStack {
-			VStack(alignment: .leading) {
-				Text(item.gasStationName.rawValue).bold()
-				Text(item.isPaidCash ? "Cash" : "Card")
+		VStack {
+			HStack(alignment: .center) {
+				Text(item.timestamp, format: Date.FormatStyle(date: .abbreviated, time: .shortened))
+					.bold()
+//				Spacer()
 			}
-			VStack(alignment: .leading) {
-				Text(item.timestamp, format: Date.FormatStyle(date: .abbreviated, time: .shortened)).bold()
-				Text("\(item.odometer) miles")
-				Text("$ " + String(item.gasPrice.roundTo(places: 2)) + " per gal")
-			}
-			.lineLimit(1)
-			VStack(alignment: .leading) {
-				Text("$ " + String(item.total.roundTo(places: 2)))
-				Text(String(item.gasMileage?.roundTo(places: 2) ?? 0) + " mpg")
-				Text(String(item.volume.roundTo(places: 2)) + " gal")
+			HStack {
+				VStack(alignment: .leading) {
+					Text(item.gasStationName.rawValue)
+					Text(item.isPaidCash ? "Cash" : "Card")
+				}
+				VStack(alignment: .leading) {
+					Text(locale.identifier)
+					if locale.identifier == "en" {
+						Text("\(item.odometer) miles")
+						Text("$ " + String(item.gasPrice.roundTo(places: 2)) + " per gal")
+					} else if locale.identifier == "ru" {
+						Text("\(item.odometer) km")
+						Text("$ " + String(item.gasPrice.roundTo(places: 2)) + " per gal")
+					}
+				}
+				.lineLimit(1)
+				VStack(alignment: .leading) {
+					Text("$ " + String(item.total.roundTo(places: 2)))
+					if locale.identifier == "en" {
+						Text(String(item.gasMileage?.roundTo(places: 2) ?? 0) + " mpg")
+						Text(String(item.volume.roundTo(places: 2)) + " gal")
+					}
+				}
 			}
 		}
 		.font(.caption)
