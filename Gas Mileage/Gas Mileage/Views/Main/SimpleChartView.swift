8b43817dfe632c5a0289e45a38c46046b0cb7c5b
Nick Shuklin <nickshuklin@icloud.com>
2024-06-22 15:33:48 +0200

    New view added

---
+++ Gas Mileage/Gas Mileage/Views/Main/SimpleChartView.swift
@@ -0,0 +1,18 @@
+//
+//  SimpleChartView.swift
+//  Gas Mileage
+//
+//  Created by Nick Shuklin on 6/22/24.
+//
+
+import SwiftUI
+
+struct SimpleChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimpleChartView()
+}
