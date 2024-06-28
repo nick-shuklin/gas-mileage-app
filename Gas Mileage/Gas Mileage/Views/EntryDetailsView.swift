53cc7be1b0191adaec05c8c271d9dece1252a424
Nick Shuklin <nickshuklin@icloud.com>
2024-06-28 19:27:03 +0200

    changes for entry details view

--- Gas Mileage/Gas Mileage/Views/EntryDetailsView.swift
+++ Gas Mileage/Gas Mileage/Views/EntryDetailsView.swift
@@ -14,67 +14,76 @@ struct EntryDetailsView: View {
 	@Binding var showTabBar: Bool
 	
 	var body: some View {
-		Form {
-			Section {
-				Text(item.timeOfFillUp, format: Date.FormatStyle(date: .abbreviated, time: .shortened))
-				Text(item.gasStationName.rawValue)
-			}
-			Section {
-				VStack {
-					HStack {
-						Text("Odometer")
-						Spacer()
-						Text("\(item.odometer) miles")
-					}
-					HStack {
-						Text("Total")
-						Spacer()
-						Text("$ " + String(item.total.roundTo(places: 2)))
-					}
-					HStack {
-						Text("Price")
-						Spacer()
-						Text("$ " + String(item.gasPrice.roundTo(places: 2)) + " per gal")
-					}
-					HStack {
-						Text("Volume")
-						Spacer()
-						Text(String(item.volume.roundTo(places: 2)) + " gal")
-					}
-					// FIXME: when formatting is fixed uncomment out this part
-//					if item.gasMileage != nil {
+		NavigationStack {
+			Form {
+				Section {
+					Text(item.timeOfFillUp, format: Date.FormatStyle(date: .abbreviated, time: .shortened))
+					Text(item.gasStationName.rawValue)
+				}
+				
+				Section {
+					VStack {
+						HStack {
+							Text("Odometer")
+							Spacer()
+							Text("\(item.odometer) miles")
+						}
+						HStack {
+							Text("Total")
+							Spacer()
+							Text("$ " + String(item.total.roundTo(places: 2)))
+						}
+						HStack {
+							Text("Price")
+							Spacer()
+							Text("$ " + String(item.gasPrice.roundTo(places: 2)) + " per gal")
+						}
+						HStack {
+							Text("Volume")
+							Spacer()
+							Text(String(item.volume.roundTo(places: 2)) + " gal")
+						}
+						// FIXME: when formatting is fixed uncomment out this part
+						//					if item.gasMileage != nil {
 						HStack {
 							Text("Gas mileage")
 							Spacer()
 							Text(String(item.gasMileage?.roundTo(places: 2) ?? 0) + " mpg")
 						}
-//					}
+						//					}
+					}
+				}
+				
+				Section {
+					Toggle(String("Tank filled up?"), isOn: $item.isFilledUp)
+					Toggle(String("Paid cash?"), isOn: $item.isPaidCash)
 				}
 			}
-			Section {
-				Toggle(String("Tank filled up?"), isOn: $item.isFilledUp)
-				Toggle(String("Paid cash?"), isOn: $item.isPaidCash)
+			.toolbar {
+				ToolbarItem(placement: .principal) {
+					Text("Entry details")
+				}
+				
+				ToolbarItem(placement: .automatic) {
+					Button {
+						showEditEntryView.toggle()
+					} label: {
+						Label("Edit", systemImage: "edit")
+					}
+					.sheet(isPresented: $showEditEntryView) {
+						EditEntryView(entry: item)
+					}
+				}
 			}
-		}
-		.onAppear {
-			withAnimation {
-				showTabBar.toggle()
+			.onAppear {
+				withAnimation {
+					showTabBar.toggle()
+				}
 			}
-		}
-		.onDisappear {
-			withAnimation {
-				showTabBar.toggle()
-			}
-		}
-		
-		HStack {
-			Button {
-				showEditEntryView.toggle()
-			} label: {
-				Label("Edit", systemImage: "edit")
-			}
-			.sheet(isPresented: $showEditEntryView) {
-				EditEntryView(entry: item)
+			.onDisappear {
+				withAnimation {
+					showTabBar.toggle()
+				}
 			}
 		}
 	}
