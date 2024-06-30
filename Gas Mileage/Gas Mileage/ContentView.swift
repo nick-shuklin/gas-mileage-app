c519b203a6165e60d8bb52d328dd29ec465556ae
Nick Shuklin <nickshuklin@icloud.com>
2024-06-30 17:24:27 +0200

    Moved logic of entry list view from content view to its own file

--- Gas Mileage/Gas Mileage/ContentView.swift
+++ Gas Mileage/Gas Mileage/ContentView.swift
@@ -9,11 +9,6 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-	@Environment(\.modelContext) private var modelContext
-	@Query(sort: \GasFillEntry.odometer, order: .reverse) private var items: [GasFillEntry]
-	@State private var showEditEntryView: Bool = false
-	@State private var showTabBar = true
-
     var body: some View {
 		TabView {
 			MainView()
@@ -21,48 +16,12 @@ struct ContentView: View {
 					Label("Main",
 						  systemImage: "house.circle.fill")
 				}
-			NavigationSplitView {
-				List {
-					ForEach(items) { item in
-						NavigationLink(value: item) {
-							EntryRowView(item: item)
-						}
-						.toolbar(showTabBar ? .visible : .hidden, for: .tabBar)
-						.listRowBackground(item.isFilledUp ? Color.green : Color.blue)
-					}
-					.onDelete(perform: deleteItems)
+			
+			ListOfEntriesView()
+				.tabItem {
+					Label("Entries",
+						  systemImage: "fuelpump.circle")
 				}
-				.navigationTitle("List of entries")
-				.navigationDestination(for: GasFillEntry.self) { item in
-					EntryDetailsView(item: item, showTabBar: $showTabBar)
-				}
-				.toolbar {
-					ToolbarItem(placement: .topBarLeading) {
-						Button {
-							showEditEntryView.toggle()
-						} label: {
-							Label("Add Item", systemImage: "plus")
-						}
-						.sheet(isPresented: $showEditEntryView) {
-							EditEntryView(entry: nil)
-						}
-					}
-					ToolbarItem(placement: .navigationBarTrailing) {
-						EditButton()
-					}
-					ToolbarItem {
-						Button(action: addItem) {
-							Label("Add Item", systemImage: "allergens")
-						}
-					}
-				}
-			} detail: {
-				Text("Select an item")
-			}
-			.tabItem {
-				Label("Entries",
-					  systemImage: "fuelpump.circle")
-			}
 			
 			GraphsView()
 				.tabItem {
@@ -72,28 +31,14 @@ struct ContentView: View {
 			
 			SettingsView()
 				.tabItem {
-					Label("Settings", systemImage: "gear")
+					Label("Settings",
+						  systemImage: "gear")
 				}
 		}
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = GasFillEntry()
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: GasFillEntry.self, inMemory: false)
+        .modelContainer(for: GasFillEntry.self, inMemory: true)
 }
