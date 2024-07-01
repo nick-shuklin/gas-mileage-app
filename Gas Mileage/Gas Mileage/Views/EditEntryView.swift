7cfdde9e318d796ba9b55fb814b12e50187b2b82
Nick Shuklin <nickshuklin@icloud.com>
2024-07-01 17:21:37 +0200

    new logic added for mpg calc

--- Gas Mileage/Gas Mileage/Views/EditEntryView.swift
+++ Gas Mileage/Gas Mileage/Views/EditEntryView.swift
@@ -9,19 +9,18 @@ import SwiftUI
 import SwiftData
 
 struct EditEntryView: View {
-//	@Bindable var item: GasFillEntry
 	// FIXME: formatter should be specified separately for each value
 	static var nf = NumberFormatter()
 	
 	let entry: GasFillEntry?
 	
-	@State private var odometer = 1111
+	@State private var odometer = ""
 	@State private var creationDate = Date()
 	@State private var timeOfFillUp = Date()
-	@State private var total = 10.40
-	@State private var gasPrice = 3.299
-	@State private var volume = 15.5
-	@State private var gasMileage = 15.7
+	@State private var total: Double = 0.0
+	@State private var gasPrice: Double = 0.0
+	@State private var volume: Double = 0.0
+	@State private var gasMileage = 0.0
 	@State private var isFilledUp = true
 	@State private var isPaidCash = false
 	@State private var selectedGasStationName: GasFillEntry.GasStationName = .chevron
@@ -32,14 +31,13 @@ struct EditEntryView: View {
 	
 	@Environment(\.dismiss) private var dismiss
 	@Environment(\.modelContext) private var modelContext
-	
-//	@Query(sort: \GasFillEntry.odometer, order: .reverse) private var items: [GasFillEntry]
+	@Query(sort: \GasFillEntry.odometer, order: .reverse) private var items: [GasFillEntry]
 	
     var body: some View {
 		NavigationStack {
 			Form {
 				Section {
-					DatePicker("Date of fill up", selection: $timeOfFillUp)
+					DatePicker("Fill up date", selection: $timeOfFillUp)
 					
 					Picker("Gas Station", selection: $selectedGasStationName) {
 						ForEach(GasFillEntry.GasStationName.allCases, id: \.self) { name in
@@ -48,57 +46,84 @@ struct EditEntryView: View {
 					}
 					.pickerStyle(.menu)
 				}
+				
 				Section {
 					VStack {
 						HStack {
 							Text("Odometer")
 							Spacer()
 							TextField("Odometer",
-									  value: $odometer,
-									  formatter: EditEntryView.nf)
+									  text: $odometer
+							)
 							.keyboardType(.numberPad)
+							.border(.secondary)
+						
+//							Text(odometer.debugDescription)
 						}
+						
 						HStack {
 							Text("Total")
 							Spacer()
 							TextField("Total",
 									  value: $total,
-									  formatter: EditEntryView.nf.totalFormat())
+									  formatter: EditEntryView.nf.totalFormat()
+							)
+//							.onTapGesture {
+//								for item in items {
+//									print(item.odometer)
+//								}
+//							}
 							.keyboardType(.decimalPad)
 							.border(.background)
+							
+//							Text(total.debugDescription)
 						}
+						
 						HStack {
 							Text("Price")
 							Spacer()
 							TextField("Price",
 									  value: $gasPrice,
-									  formatter: EditEntryView.nf.priceFormat())
+									  formatter: EditEntryView.nf.priceFormat()
+							)
+							.onChange(of: gasPrice) {
+								volume = (total / gasPrice).roundTo(places: 2)
+							}
+
 							.keyboardType(.decimalPad)
+							
+//							Text(gasPrice.debugDescription)
 						}
+						
 						HStack {
 							Text("Volume")
 							Spacer()
 							TextField("Enter volume in gallons",
 									  value: $volume,
-									  formatter: EditEntryView.nf.totalFormat())
+									  formatter: EditEntryView.nf.totalFormat()
+							)
+							.onChange(of: volume) {
+								total = (volume * gasPrice).roundTo(places: 2)
+							}
 							.keyboardType(.decimalPad)
-						}
-						HStack {
-							Text("Gas mileage")
-							Spacer()
-							TextField("miles per gallon",
-									  value: $gasMileage,
-									  formatter: EditEntryView.nf.totalFormat())
-							.keyboardType(.decimalPad)
+							
+//							Text(volume.debugDescription)
 						}
 					}
 				}
+				
 				Section {
 					Toggle(String("Tank filled up?"), isOn: $isFilledUp)
 					Toggle(String("Paid cash?"), isOn: $isPaidCash)
 				}
 			}
 			.toolbar {
+				ToolbarItem(placement: .cancellationAction) {
+					Button("Cancel", role: .cancel) {
+						dismiss()
+					}
+				}
+				
 				ToolbarItem(placement: .principal) {
 					Text(editorTitle)
 				}
@@ -106,36 +131,50 @@ struct EditEntryView: View {
 				ToolbarItem(placement: .confirmationAction) {
 					Button("Save") {
 						withAnimation {
+							calculateGasMileage()
 							save()
 							dismiss()
 						}
 					}
-					// Require a category to save changes.
-//					.disabled(gasStationName. == nil)
-				}
-				
-				ToolbarItem(placement: .cancellationAction) {
-					Button("Cancel", role: .cancel) {
-						dismiss()
-					}
+					.disabled(odometer == "")
 				}
 			}
 			.onAppear {
 				if let entry {
-					// Edit the incoming entry.
-					odometer = entry.odometer
+					odometer = String(entry.odometer)
 					timeOfFillUp = entry.timeOfFillUp
+					total = entry.total
+					gasPrice = entry.gasPrice
+					volume = entry.volume
+					gasMileage = entry.gasMileage
 					isFilledUp = entry.isFilledUp
+					isPaidCash = entry.isPaidCash
+					selectedGasStationName = entry.gasStationName
 				}
 			}
-			.padding()
 		}
+		.padding()
     }
+	
+	private func calculateGasMileage() {
+		if isFilledUp {
+			for item in items {
+				if (item.odometer < Int(odometer)!) && item.isFilledUp {
+					gasMileage = Double(Int(odometer)! - item.odometer) / volume
+				}
+			}
+		} else {
+			for item in items {
+				if (item.odometer < Int(odometer)!){
+					gasMileage = item.gasMileage
+				}
+			}
+		}
+	}
 	
 	private func save() {
 		if let entry {
-			// Edit the entry.
-			entry.odometer = odometer
+			entry.odometer = Int(odometer)!
 			entry.timeOfFillUp = timeOfFillUp
 			entry.total = total
 			entry.gasPrice = gasPrice
@@ -145,8 +184,7 @@ struct EditEntryView: View {
 			entry.isPaidCash = isPaidCash
 			entry.gasStationName = selectedGasStationName
 		} else {
-			// Add an entry.
-			let newEntry = GasFillEntry(odometer: odometer,
+			let newEntry = GasFillEntry(odometer: Int(odometer)!,
 										timeOfFillUp: timeOfFillUp,
 										total: total,
 										gasPrice: gasPrice,
