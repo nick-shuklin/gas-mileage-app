2bef54571602398a51352ee371583e48fa09c8f5
Nick Shuklin <nickshuklin@icloud.com>
2024-07-03 17:13:39 +0200

    Added new toggle to the settings page

--- Gas Mileage/Gas Mileage/Views/SettingsView.swift
+++ Gas Mileage/Gas Mileage/Views/SettingsView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct SettingsView: View {
+	@State private var isMetric = true
+	
     var body: some View {
-		Form {
-			Section("About") {
-				Text("Version \(AppVersionProvider.appVersion())")
+		NavigationStack {
+			Text("Settings")
+				.font(.headline)
+			
+			Form {
+				Section("Unit settings") {
+					Toggle(isOn: $isMetric) {
+						Label("Imperial or Metric",
+							  systemImage: isMetric ? "m.square.fill" : "i.square.fill")
+					}
+					.toggleStyle(SwitchToggleStyle(tint: .cyan))
+				}
+				
+				Section("About") {
+					Text("Version \(AppVersionProvider.appVersion())")
+				}
 			}
 		}
     }
+}
+
+#Preview {
+	SettingsView()
 }
