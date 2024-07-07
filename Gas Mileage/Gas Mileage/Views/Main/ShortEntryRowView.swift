eb2f2d5490befbcff833c55d9069a1c155b36090
Nick Shuklin <nickshuklin@icloud.com>
2024-07-07 16:17:08 +0200

    Added proper preview in one of the veiws

--- Gas Mileage/Gas Mileage/Views/Main/ShortEntryRowView.swift
+++ Gas Mileage/Gas Mileage/Views/Main/ShortEntryRowView.swift
@@ -12,7 +12,7 @@ struct ShortEntryRowView: View {
 	
 	var body: some View {
 		HStack {
-			Text("Logo")
+			Text("Logo") // here will be small gas station logo pic
 			Divider()
 			Text(item.creationDate, format: Date.FormatStyle(date: .numeric, time: .shortened))
 				.bold()
@@ -23,3 +23,9 @@ struct ShortEntryRowView: View {
 		.font(.caption)
 	}
 }
+
+#Preview {
+	ShortEntryRowView(item: GasFillEntry())
+		.modelContainer(for: GasFillEntry.self, inMemory: true)
+}
+
