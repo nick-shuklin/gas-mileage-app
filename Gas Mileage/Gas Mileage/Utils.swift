3436470389c7d133e78c1196ed8ae492a4ed7557
Nick Shuklin <nickshuklin@icloud.com>
2024-07-25 18:26:19 +0200

    Updated utils with more picker options

--- Gas Mileage/Gas Mileage/Utils.swift
+++ Gas Mileage/Gas Mileage/Utils.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 enum TimeRange {
 	case last30Days
+	case last3months
 	case last12Months
 	case ytd
+	case all
 }
 
 struct TimeRangePicker: View {
@@ -18,10 +20,12 @@ struct TimeRangePicker: View {
 
 	var body: some View {
 		Picker(selection: $value.animation(.easeInOut), label: EmptyView()) {
-			Text("30 Days").tag(TimeRange.last30Days)
-			Text("12 Months").tag(TimeRange.last12Months)
+			Text("30D").tag(TimeRange.last30Days)
+			Text("3M").tag(TimeRange.last3months)
+			Text("12M").tag(TimeRange.last12Months)
 			Text("YTD").tag(TimeRange.ytd)
+			Text("ALL").tag(TimeRange.all)
 		}
-		.pickerStyle(.segmented)
+		.pickerStyle(.palette)
 	}
 }
