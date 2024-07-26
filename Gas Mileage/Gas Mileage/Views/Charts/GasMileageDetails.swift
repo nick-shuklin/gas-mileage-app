61b03a09423dd6205dfa056c1aba968aebf9ff62
Nick Shuklin <nickshuklin@icloud.com>
2024-07-26 15:38:47 +0200

    gas mileage chart details view now have 3 options implemented

--- Gas Mileage/Gas Mileage/Views/Charts/GasMileageDetails.swift
+++ Gas Mileage/Gas Mileage/Views/Charts/GasMileageDetails.swift
@@ -20,24 +20,22 @@ struct GasMileageDetails: View {
 				
 				switch timeRange {
 					case .last30Days:
-						Text("adadfads")
-							.font(.title2.bold())
-							.foregroundColor(.primary)
-						
-						Text("qewewwqewq")
-							.font(.callout)
-							.foregroundStyle(.secondary)
+//						Text("adadfads")
+//							.font(.title2.bold())
+//							.foregroundColor(.primary)
+//						
+//						Text("qewewwqewq")
+//							.font(.callout)
+//							.foregroundStyle(.secondary)
 						
 						GasMileageChart30Days()
 							.frame(height: 240)
-					case .last3months:
-						<#code#>
-					case .last12Months:
-						<#code#>
-					case .ytd:
-						<#code#>
-					case .all:
-						<#code#>
+					case .last90Days:
+						GasMileageChart90Days()
+							.frame(height: 240)
+					default:
+						GasMileageChartAll()
+							.frame(height: 240)
 				}
 			}
 		}
@@ -45,27 +43,24 @@ struct GasMileageDetails: View {
 }
 
 struct GasMileageChart30Days: View {
-	@Query(MainView.fetchDescriptor) private var items: [GasFillEntry]
-	@Environment(\.modelContext) private var modelContext
+	@Query(GasMileageChart30Days.fetchDescriptor) private var items: [GasFillEntry]
 
-	static let amountOfEntriesToDisplay = 10
-	static var fetchDescriptor: FetchDescriptor<GasFillEntry> {
-		var descriptor = FetchDescriptor<GasFillEntry>(
-			predicate: #Predicate { $0.f == true },
-			sortBy: [SortDescriptor(\.odometer, order: .reverse)]
-		)
-		descriptor.fetchLimit = amountOfEntriesToDisplay
-		return descriptor
+	static func predicate() -> Predicate<GasFillEntry> {
+		let calendar = Calendar.autoupdatingCurrent
+		let end = calendar.startOfDay(for: Date())
+		let start = calendar.date(byAdding: .init(day: -30), to: end) ?? end
+	
+		return #Predicate<GasFillEntry> { entry in
+			(entry.fillUpDate > start && entry.fillUpDate < end)
+		}
 	}
 	
-	var gradient: LinearGradient {
-		LinearGradient(gradient: Gradient(colors: [
-			Color(.purple).opacity(0.4),
-			Color(.purple).opacity(0.05)
-		]),
-					   startPoint: .top,
-					   endPoint: .bottom
+	static var fetchDescriptor: FetchDescriptor<GasFillEntry> {
+		let descriptor = FetchDescriptor<GasFillEntry>(
+			predicate: predicate(),
+			sortBy: [SortDescriptor(\.odometer, order: .reverse)]
 		)
+		return descriptor
 	}
 	
 	var body: some View {
@@ -84,13 +79,114 @@ struct GasMileageChart30Days: View {
 					x: .value("Date", item.fillUpDate, unit: .day),
 					y: .value("Gas mileage", item.gasMileage)
 				)
-				.foregroundStyle(gradient)
+				.foregroundStyle(chartsGradient)
+			}
+			.interpolationMethod(.catmullRom)
+		}
+		.chartXAxis {
+			AxisMarks(values: .stride(by: .day, count: 7)) { _ in
+				AxisGridLine()
+				AxisTick()
+				AxisValueLabel(format: .dateTime.month().day(), centered: false)
+			}
+		}
+		.chartYAxis {
+			AxisMarks(values: [0, 10, 20, 30, 40]) // TODO: add computed var based of max and min values
+		}
+		// This should be optional depending on amount of entries
+//		.chartScrollableAxes(.horizontal)
+//		.chartXVisibleDomain(length: 3600 * 24 * 15)
+	}
+}
+
+struct GasMileageChart90Days: View {
+	@Query(GasMileageChart90Days.fetchDescriptor) private var items: [GasFillEntry]
+
+	static func predicate() -> Predicate<GasFillEntry> {
+		let calendar = Calendar.autoupdatingCurrent
+		let end = calendar.startOfDay(for: Date())
+		let start = calendar.date(byAdding: .init(day: -90), to: end) ?? end
+	
+		return #Predicate<GasFillEntry> { entry in
+			(entry.fillUpDate > start && entry.fillUpDate < end)
+		}
+	}
+	
+	static var fetchDescriptor: FetchDescriptor<GasFillEntry> {
+		let descriptor = FetchDescriptor<GasFillEntry>(
+			predicate: predicate(),
+			sortBy: [SortDescriptor(\.odometer, order: .reverse)]
+		)
+		return descriptor
+	}
+	
+	var body: some View {
+		
+		Chart {
+			ForEach(items) { item in
+				LineMark(
+					x: .value("Date", item.fillUpDate, unit: .day),
+					y: .value("Gas mileage", item.gasMileage)
+				)
+				.lineStyle(StrokeStyle(lineWidth: 1))
+				.foregroundStyle(.purple)
+				.symbol(Circle())
+				
+				AreaMark(
+					x: .value("Date", item.fillUpDate, unit: .day),
+					y: .value("Gas mileage", item.gasMileage)
+				)
+				.foregroundStyle(chartsGradient)
+			}
+			.interpolationMethod(.catmullRom)
+		}
+		.chartXAxis {
+			AxisMarks(values: .stride(by: .month)) { _ in
+				AxisGridLine()
+				AxisTick()
+				AxisValueLabel(format: .dateTime.month(.abbreviated), centered: false)
+			}
+		}
+		.chartYAxis {
+			AxisMarks(values: [0, 10, 20, 30, 40]) // TODO: add computed var based of max and min values
+		}
+		.chartScrollTargetBehavior(
+			.valueAligned(
+				matching: .init(day: 1),
+				majorAlignment: .matching(.init(month: 1))
+			)
+		)
+		.chartScrollableAxes(.horizontal)
+		.chartXVisibleDomain(length: 3600 * 24 * 90)
+	}
+}
+
+struct GasMileageChartAll: View {
+	@Query(sort: \GasFillEntry.odometer, order: .reverse) private var items: [GasFillEntry]
+	
+	var body: some View {
+		
+		Chart {
+			ForEach(items) { item in
+				LineMark(
+					x: .value("Date", item.fillUpDate, unit: .day),
+					y: .value("Gas mileage", item.gasMileage)
+				)
+				.lineStyle(StrokeStyle(lineWidth: 1))
+				.foregroundStyle(.purple)
+				.symbol(Circle())
+				
+				AreaMark(
+					x: .value("Date", item.fillUpDate, unit: .day),
+					y: .value("Gas mileage", item.gasMileage)
+				)
+				.foregroundStyle(chartsGradient)
 			}
 			.interpolationMethod(.catmullRom)
 		}
 		.chartScrollableAxes(.horizontal)
 		.chartXAxis {
-			AxisMarks(values: .stride(by: .month)) { _ in
+			AxisMarks(values: .stride(by: .month)) { value in
 				AxisGridLine()
 				AxisTick()
 				AxisValueLabel(format: .dateTime.month(.abbreviated), centered: false)
