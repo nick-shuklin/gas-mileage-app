bc825fd66e2593f2e86dec93a453143ba120d131
Nick Shuklin <nickshuklin@icloud.com>
2024-07-31 13:55:17 +0200

    New section added to Settings page

--- Gas Mileage/Gas Mileage/Views/SettingsView.swift
+++ Gas Mileage/Gas Mileage/Views/SettingsView.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct SettingsView: View {
 	@State private var isMetric = true
+	@State private var inProgress = false
+	
+	@Environment(\.modelContext) private var modelContext
+	@Query(sort: \GasFillEntry.odometer, order: .reverse) private var items: [GasFillEntry]
 	
     var body: some View {
 		ZStack {
@@ -30,6 +35,15 @@ struct SettingsView: View {
 						Text("Unit settings")
 					} footer: {
 						Text("Default settings applied based on iPhone settings. Metric: km for odometer, liters per 100km for gas mileage. Imperial: ml and miles per gallon (mpg)")
+					}
+					
+					Section() {
+						Toggle(String("Recalculate MPG in ALL entries"), isOn: $inProgress)
+							.toggleStyle(SwitchToggleStyle(tint: .cyan))
+					} header: {
+						Text("Utilities")
+					} footer: {
+						Text("To forcefully recalculate each entry gas mileage data turn on toggle above and wait until it automatically turns back off")
 					}
 					
 					Section("About") {
