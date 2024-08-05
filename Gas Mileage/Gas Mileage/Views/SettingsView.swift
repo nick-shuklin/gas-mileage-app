2dfbcaca0312e042535a0fb61834799ffd8d1d70
Nick Shuklin <nickshuklin@icloud.com>
2024-08-05 12:32:07 +0200

    now settings page has logic to recalculate all gas mileage data by going through each entry

--- Gas Mileage/Gas Mileage/Views/SettingsView.swift
+++ Gas Mileage/Gas Mileage/Views/SettingsView.swift
@@ -11,9 +11,7 @@ import SwiftData
 struct SettingsView: View {
 	@State private var isMetric = true
 	@State private var inProgress = false
-	
-	@Environment(\.modelContext) private var modelContext
-	@Query(sort: \GasFillEntry.odometer, order: .reverse) private var items: [GasFillEntry]
+	@Query(sort: \GasFillEntry.odometer, order: .forward) var items: [GasFillEntry]
 	
     var body: some View {
 		ZStack {
@@ -40,6 +38,9 @@ struct SettingsView: View {
 					Section() {
 						Toggle(String("Recalculate MPG in ALL entries"), isOn: $inProgress)
 							.toggleStyle(SwitchToggleStyle(tint: .cyan))
+							.onChange(of: inProgress) {
+								recalculateGasMileage()
+							}
 					} header: {
 						Text("Utilities")
 					} footer: {
@@ -53,8 +54,32 @@ struct SettingsView: View {
 			}
 		}
     }
-}
+	
+	// TODO: move this outside of the view, add small 0.5 sec delay for animation
+	private func recalculateGasMileage() {
+		if inProgress {
+			var data: (odometer: Int, accumulatedVolume: Double, gasMileage: Double) = (0, 0, 1)
+			
+			for (i, item) in items.enumerated() {
+				print("bla \(i)")
+				
+				if i == 0 {
+					item.gasMileage = 1
+				} else {
+					if item.isFilledUp {
+						item.gasMileage = Double(item.odometer - data.odometer) / (data.accumulatedVolume + item.volume)
+						data.odometer = item.odometer
+						data.accumulatedVolume = 0
+						data.gasMileage = item.gasMileage
+					} else {
+						item.gasMileage = data.gasMileage
+						data.accumulatedVolume += item.volume
+					}
+				}
+			}
 
-#Preview {
-	SettingsView()
+			inProgress.toggle()
+			print("---***---")
+		}
+	}
 }
