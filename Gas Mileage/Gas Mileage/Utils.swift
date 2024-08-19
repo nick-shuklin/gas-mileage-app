c736fc97f9202e1c8d09810df6ee04396630e6e6
Nick Shuklin <nickshuklin@icloud.com>
2024-08-19 20:23:05 +0200

    Moved background shadow to Utils

--- Gas Mileage/Gas Mileage/Utils.swift
+++ Gas Mileage/Gas Mileage/Utils.swift
@@ -74,3 +74,50 @@ var fetchDescriptorLast10: FetchDescriptor<GasFillEntry> {
 	descriptor.fetchLimit = 10
 	return descriptor
 }
+
+struct backGroundSquareShapedShadow: View {
+	let radius: CGFloat = 7
+	let modifier: CGFloat = 8
+
+	let innerColor1 = Color(hex: "FFFFFF").opacity(0.64)
+	let innerColor2 = Color(hex: "0D2750").opacity(0.16)
+	/// B5BFC6 FAFBFF E4EBF1 FFFFFF 6E7F8D 0D2750 161B1D EFF2F9
+	var innerBlur1: CGFloat = 43
+	var innerBlur2: CGFloat = 48
+	var innerXY1: CGFloat = -31
+	var innerXY2: CGFloat = 26
+
+	let dropColor1 = Color(hex: "0D2750").opacity(0.16)
+	let dropColor2 = Color(hex: "FFFFFF")
+	var dropBlur1: CGFloat = 50
+	var dropBlur2: CGFloat = 45
+	var dropXY1: CGFloat = 28
+	var dropXY2: CGFloat = -23
+
+	var body: some View {
+		RoundedRectangle(cornerRadius: 5)
+			.fill(
+				.shadow(.inner(color: innerColor1,
+							   radius: innerBlur1/modifier,
+							   x: innerXY1/modifier,
+							   y: innerXY1/modifier)
+				)
+				.shadow(.inner(color: innerColor2,
+							   radius: innerBlur2/modifier,
+							   x: innerXY2/modifier,
+							   y: innerXY2/modifier)
+				)
+				.shadow(.drop(color: dropColor1,
+							  radius: dropBlur1/modifier,
+							  x: dropXY1/modifier,
+							  y: dropXY1/modifier)
+				)
+				.shadow(.drop(color: dropColor2,
+							  radius: dropBlur2/modifier,
+							  x: dropXY2/modifier,
+							  y: dropXY2/modifier)
+				)
+			)
+			.foregroundColor(Color.background)
+	}
+}
