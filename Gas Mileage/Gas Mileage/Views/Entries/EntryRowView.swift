6984710da97d048ca82c7cf09005f85a3e27e1bb
Nick Shuklin <nickshuklin@icloud.com>
2024-08-26 13:53:44 +0200

    updated entry row view with new logoview

--- Gas Mileage/Gas Mileage/Views/Entries/EntryRowView.swift
+++ Gas Mileage/Gas Mileage/Views/Entries/EntryRowView.swift
@@ -6,31 +6,24 @@ struct EntryRowView: View {
 
     var body: some View {
 		HStack {
-			VStack(alignment: .leading) {
-				Text(item.gasStationName.rawValue).bold()
-//				Text(item.isPaidCash ? "Cash" : "Card")
-				Text(item.isFilledUp ? "Full" : "")
-			}
-			
+			LogoView(logoName: item.gasStationName.rawValue)
 			Spacer()
-			
 			VStack(alignment: .leading) {
-				Text(item.fillUpDate, format: Date.FormatStyle(date: .abbreviated, time: .shortened)).bold()
+				Text(item.fillUpDate,
+					 format: Date.FormatStyle(date: .abbreviated,
+											  time: .none))
+				.bold()
 				Text("\(item.odometer) miles")
 				Text("$" + String(item.gasPrice.roundTo(places: 2)) + " per gal")
 			}
 			.lineLimit(1)
-			
 			Spacer()
-			
 			VStack(alignment: .leading) {
 				Text("$" + String(item.total.roundTo(places: 2)))
 				Text(String(item.gasMileage.roundTo(places: 2)) + " mpg").bold()
 				Text(String(item.volume.roundTo(places: 2)) + " gal")
 			}
-			
 			Spacer()
-			
 			Image(systemName: "chevron.right")
 				.background(
 					backGroundSquareShapedShadow()
