cbff991b8d22dfa8013e090d4c5df04fefc4d4ad
Nick Shuklin <nickshuklin@icloud.com>
2024-08-26 13:10:14 +0200

    simplified code in logoview

--- Gas Mileage/Gas Mileage/Views/LogoView.swift
+++ Gas Mileage/Gas Mileage/Views/LogoView.swift
@@ -11,29 +11,16 @@ struct LogoView: View {
 					   height: logoSize + 2)
 				.cornerRadius(5)
 			
-			if let uiImage = UIImage(named: logoName) {
-				Image(uiImage: uiImage)
-					.renderingMode(.original)
-					.resizable()
-					.saturation(0)
-					.contrast(0.7)
-					.aspectRatio(contentMode: .fit)
-					.frame(width: logoSize,
-						   height: logoSize)
-					.cornerRadius(5)
-					.clipped()
-			} else {
-				Image(uiImage: UIImage(named: "Default")!)
-					.renderingMode(.original)
-					.resizable()
-					.saturation(0)
-					.contrast(0.7)
-					.aspectRatio(contentMode: .fit)
-					.frame(width: logoSize,
-						   height: logoSize)
-					.cornerRadius(5)
-					.clipped()
-			}
+			Image(uiImage: UIImage(named: logoName) ?? UIImage(named: "Default")!)
+				.renderingMode(.original)
+				.resizable()
+				.saturation(0)
+				.contrast(0.7)
+				.aspectRatio(contentMode: .fit)
+				.frame(width: logoSize,
+					   height: logoSize)
+				.cornerRadius(5)
+				.clipped()
 		}
 	}
 }
