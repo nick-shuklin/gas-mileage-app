5d4aa189ddb4e314eccfa12201fa0fffe12c4185
Nick Shuklin <nickshuklin@icloud.com>
2024-09-03 15:36:51 +0200

    WIP total expenses details

--- Gas Mileage/Gas Mileage/Views/Charts/TotalExpensesDetails.swift
+++ Gas Mileage/Gas Mileage/Views/Charts/TotalExpensesDetails.swift
@@ -1,1 +1,103 @@
+import Charts
+import SwiftUI
+import SwiftData
 
+struct TotalExpensesDetails: View {
+	@State private var timeRange: TimeRangeTotalExpensesChart = .last3months
+	
+	var body: some View {
+		VStack(alignment: .leading) {
+			TimeRangeTotalExpensesChartPicker(value: $timeRange)
+				.padding(.bottom)
+			
+			switch timeRange {
+				case .last3months:
+//						Text("adadfads")
+//							.font(.title2.bold())
+//							.foregroundColor(.primary)
+//
+//						Text("qewewwqewq")
+//							.font(.callout)
+//							.foregroundStyle(.secondary)
+					
+					TotalExpensesChart3months()
+				case .ytd:
+					TotalExpensesChartYTD()
+				case .all:
+					TotalExpensesChartAll()
+			}
+		}
+	}
+}
+
+struct TotalExpensesChart3months: View {
+	@Query(fetchDescriptor30Days) private var items: [GasFillEntry]
+	
+	var body: some View {
+		let expenses = groupEntriesByMonthAndCalculateTotal(items)
+		
+		Chart {
+			ForEach(expenses.keys.sorted(), id: \.self) { date in
+				if let amount = expenses[date] {
+					SectorMark(
+						angle: .value("Amount", amount),
+						innerRadius: .ratio(0.6),
+						angularInset: 2
+					)
+					.foregroundStyle(by: .value("Month", date))
+					.cornerRadius(5)
+				}
+			}
+		}
+		.chartXAxis(.hidden)
+		.chartYAxis(.hidden)
+	}
+}
+
+struct TotalExpensesChartYTD: View {
+	@Query(fetchDescriptor90Days) private var items: [GasFillEntry]
+	
+	var body: some View {
+		let expenses = groupEntriesByMonthAndCalculateTotal(items)
+		
+		Chart {
+			ForEach(expenses.keys.sorted(), id: \.self) { date in
+				if let amount = expenses[date] {
+					SectorMark(
+						angle: .value("Amount", amount),
+						innerRadius: .ratio(0.6),
+						angularInset: 2
+					)
+					.foregroundStyle(by: .value("Month", date))
+					.cornerRadius(5)
+				}
+			}
+		}
+		.chartXAxis(.hidden)
+		.chartYAxis(.hidden)
+	}
+}
+
+struct TotalExpensesChartAll: View {
+	@Query(fetchDescriptorAll) private var items: [GasFillEntry]
+	
+	var body: some View {
+		let expenses = groupEntriesByMonthAndCalculateTotal(items)
+		
+		Chart {
+			ForEach(expenses.keys.sorted(), id: \.self) { date in
+				if let amount = expenses[date] {
+					SectorMark(
+						angle: .value("Amount", amount),
+						innerRadius: .ratio(0.6),
+						angularInset: 2
+					)
+					.foregroundStyle(by: .value("Month", date))
+					.cornerRadius(5)
+				}
+			}
+		}
+		.chartXAxis(.hidden)
+		.chartYAxis(.hidden)
+	}
+}
