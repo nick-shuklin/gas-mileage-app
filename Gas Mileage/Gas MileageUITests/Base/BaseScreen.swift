712bb4696cc75b1239c2bbb9535f1654f6fef7eb
Nick Shuklin <nickshuklin@icloud.com>
2024-10-25 19:47:33 -0700

    BaseScreen now has runActivity method and icons for different types of messages

--- Gas Mileage/Gas MileageUITests/Base/BaseScreen.swift
+++ Gas Mileage/Gas MileageUITests/Base/BaseScreen.swift
@@ -7,6 +7,8 @@ protocol BaseScreen {
 }
 
 extension BaseScreen {
+	
+	// MARK: - Apps
 	var app: XCUIApplication {
 		XCUIApplication()
 	}
@@ -21,6 +23,29 @@ extension BaseScreen {
 	
 	var settingsApp: XCUIApplication {
 		return XCUIApplication(bundleIdentifier: UITestConstant.AppBundleIDs.settings)
+	}
+	
+	// MARK: - activity message addons
+	var assert: String {
+		return Icons.assert.rawValue
+	}
+	
+	var err: String {
+		return Icons.error.rawValue
+	}
+	
+	var screen: String {
+		return Icons.screen.rawValue
+	}
+	
+	var step: String {
+		return Icons.step.rawValue
+	}
+	
+	func runActivity(_ icon: Icons, _ named: String, block: () -> Void) {
+		XCTContext.runActivity(named: "\(icon.rawValue) \(named)", block: {_ in
+			block()
+		})
 	}
 	
 	@discardableResult
