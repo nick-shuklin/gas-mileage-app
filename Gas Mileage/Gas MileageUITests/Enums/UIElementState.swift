e87b197e2c76fd21db0b54cb9b30b3bbfbe642a8
Nick Shuklin <nickshuklin@icloud.com>
2024-10-26 12:08:44 -0700

    one more enum added

---
+++ Gas Mileage/Gas MileageUITests/Enums/UIElementState.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum UIElementState: String {
+	case exists = "exists"
+	case hittable = "isHittable"
+	case enabled = "isEnabled"
+	case selected = "isSelected"
+	case focused = "hasFocus"
+}
