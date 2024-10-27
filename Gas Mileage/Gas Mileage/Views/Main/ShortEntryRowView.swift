361cec7c7068d9d97925c1398c032e5a234a5967
Nick Shuklin <nickshuklin@icloud.com>
2024-10-26 20:48:26 -0700

    added identifiers to elements on short entry row view

--- Gas Mileage/Gas Mileage/Views/Main/ShortEntryRowView.swift
+++ Gas Mileage/Gas Mileage/Views/Main/ShortEntryRowView.swift
@@ -6,15 +6,19 @@ struct ShortEntryRowView: View {
 	var body: some View {
 		HStack {
 			LogoView(logoName: item.gasStationName.rawValue)
+				.accessibilityIdentifier("short_entry_row_logo_\(item.odometer)")
 			Spacer()
 			Text(item.creationDate, 
 				 format: Date.FormatStyle(date: .numeric, time: .none))
 				.bold()
 				.lineLimit(1)
+				.accessibilityIdentifier("short_entry_row_creation_date_\(item.odometer)")
 			Spacer()
 			Text("$" + String(item.gasPrice.roundTo(places: 2)) + "/gal")
+				.accessibilityIdentifier("short_entry_row_price_\(item.odometer)")
 			Spacer()
 			Text("$" + String(item.total.roundTo(places: 2)))
+				.accessibilityIdentifier("short_entry_row_total_\(item.odometer)")
 		}
 		.font(.caption)
 	}
