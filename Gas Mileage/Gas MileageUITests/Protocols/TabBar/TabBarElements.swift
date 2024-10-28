187068fa5eb50757bbd7a1a4b0b6067a40e6c3fb
Nick Shuklin <nickshuklin@icloud.com>
2024-10-28 13:17:31 -0700

    updated protocol again

--- Gas Mileage/Gas MileageUITests/Protocols/TabBar/TabBarElements.swift
+++ Gas Mileage/Gas MileageUITests/Protocols/TabBar/TabBarElements.swift
@@ -15,18 +15,21 @@ extension TabBarElements {
 	}
 	
 	var mainTabBarButton: XCUIElement {
-		return tabBar.buttons[AccIDs.TabBar.mainTabBarButton.rawValue].firstMatch
+		// Since the app complexity is simple, we search for all elements directly in the entire view hierarchy (VH).
+		// This is an example of a hybrid approach to ensure robustness in cases where the parent element might not be found.
+		let element = tabBar.buttons[AccIDs.TabBar.mainTabBarButton.rawValue].firstMatch
+		return element.exists ? element : app.buttons[AccIDs.TabBar.mainTabBarButton.rawValue].firstMatch
 	}
 	
 	var entriesTabBarButton: XCUIElement {
-		return tabBar.buttons[AccIDs.TabBar.entriesTabBarButton.rawValue].firstMatch
+		return app.buttons[AccIDs.TabBar.entriesTabBarButton.rawValue].firstMatch
 	}
 	
 	var chartsTabBarButton: XCUIElement {
-		return tabBar.buttons[AccIDs.TabBar.chartsTabBarButton.rawValue].firstMatch
+		return app.buttons[AccIDs.TabBar.chartsTabBarButton.rawValue].firstMatch
 	}
 	
 	var settingsTabBarButton: XCUIElement {
-		return tabBar.buttons[AccIDs.TabBar.settingsTabBarButton.rawValue].firstMatch
+		return app.buttons[AccIDs.TabBar.settingsTabBarButton.rawValue].firstMatch
 	}
 }
