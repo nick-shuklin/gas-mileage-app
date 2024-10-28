d106dba08a7dfc73406379d1932b304a69423da1
Nick Shuklin <nickshuklin@icloud.com>
2024-10-28 16:41:32 -0700

    added accIDs to the elements in entry row view

--- Gas Mileage/Gas Mileage/Views/Entries/EntryRowView.swift
+++ Gas Mileage/Gas Mileage/Views/Entries/EntryRowView.swift
@@ -7,21 +7,28 @@ struct EntryRowView: View {
     var body: some View {
 		HStack {
 			LogoView(logoName: item.gasStationName.rawValue)
+				.accessibilityIdentifier("entry_row_logo_\(item.odometer)")
 			Spacer()
 			VStack(alignment: .leading) {
 				Text(item.fillUpDate,
 					 format: Date.FormatStyle(date: .abbreviated,
 											  time: .none))
 				.bold()
+				.accessibilityIdentifier("entry_row_fillupdate_\(item.odometer)")
 				Text("\(item.odometer) miles")
+					.accessibilityIdentifier("entry_row_odometer_\(item.odometer)")
 				Text("$" + String(item.gasPrice.roundTo(places: 2)) + " per gal")
+					.accessibilityIdentifier("entry_row_gasprice_\(item.odometer)")
 			}
 			.lineLimit(1)
 			Spacer()
 			VStack(alignment: .leading) {
 				Text("$" + String(item.total.roundTo(places: 2)))
+					.accessibilityIdentifier("entry_row_total_\(item.odometer)")
 				Text(String(item.gasMileage.roundTo(places: 2)) + " mpg").bold()
+					.accessibilityIdentifier("entry_row_gasmileage_\(item.odometer)")
 				Text(String(item.volume.roundTo(places: 2)) + " gal")
+					.accessibilityIdentifier("entry_row_volume_\(item.odometer)")
 			}
 			Spacer()
 			Image(systemName: "chevron.right")
