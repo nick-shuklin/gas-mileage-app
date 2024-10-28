8bb424a143e8cf06df5f65548752c8ff07820861
Nick Shuklin <nickshuklin@icloud.com>
2024-10-28 12:52:26 -0700

    New enum for accIDs

---
+++ Gas Mileage/Gas MileageUITests/Enums/AccIDs.swift
@@ -0,0 +1,15 @@
+enum AccIDs {
+	enum HomeScreen: String {
+		case mainTabView = "main_tab"
+		case chartView = "ytd_chart_view"
+		case scrollView = "scroll_view"
+		case last10EntriesLabel = "last_10_entries_label"
+	}
+	
+	enum TabBar: String {
+		case mainTabBarButton = "house"
+		case entriesTabBarButton = "fuelpump"
+		case chartsTabBarButton = "waveform.and.person.filled"
+		case settingsTabBarButton = "folder.badge.gearshape"
+	}
+}
