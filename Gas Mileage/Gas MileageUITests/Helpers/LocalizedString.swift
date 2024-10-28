e0177ea0039e2568418d91acc188868eb294fcb9
Nick Shuklin <nickshuklin@icloud.com>
2024-10-27 21:44:15 -0700

    New helper method localized string added

---
+++ Gas Mileage/Gas MileageUITests/Helpers/LocalizedString.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+struct LocalizedString {
+	static func string(forKey key: String,
+					   locale: String = "en_US") -> String {
+		let locale = ProcessInfo.processInfo.environment["TEST_LOCALE"] ?? "en_US"
+		
+		guard let path = Bundle(for: BaseUITest.self).path(forResource: locale, ofType: "strings") else {
+			print("Path not found for resource: \(locale)")
+			return key
+		}
+		
+		guard let localizedDictionary = NSDictionary(contentsOfFile: path) as? [String: String] else {
+			print("Failed to load dictionary from file at path: \(path)")
+			return key
+		}
+		
+		return localizedDictionary[key] ?? key
+	}
+}
