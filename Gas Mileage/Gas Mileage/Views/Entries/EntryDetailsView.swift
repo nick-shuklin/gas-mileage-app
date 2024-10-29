527a868783b7a4952ff966a276d06dbfa96c91ce
Nick Shuklin <nickshuklin@icloud.com>
2024-10-29 13:45:08 -0700

    added missing accIDs on entry details view

--- Gas Mileage/Gas Mileage/Views/Entries/EntryDetailsView.swift
+++ Gas Mileage/Gas Mileage/Views/Entries/EntryDetailsView.swift
@@ -10,23 +10,32 @@ struct EntryDetailsView: View {
 		Form {
 			Section {
 				Text(item.fillUpDate, format: Date.FormatStyle(date: .abbreviated, time: .shortened))
+					.accessibilityIdentifier("fill_up_date")
 				Text(item.gasStationName.rawValue)
+					.accessibilityIdentifier("gas_station_name")
 			}
 			
 			Section {
 				VStack {
 					DetailRow(label: "Odometer", value: "\(item.odometer) miles")
+						.accessibilityIdentifier("odometer")
 					DetailRow(label: "Total", value: "$\(item.total.roundTo(places: 2))")
+						.accessibilityIdentifier("total")
 					DetailRow(label: "Price", value: "$\(item.gasPrice.roundTo(places: 2)) per gal")
+						.accessibilityIdentifier("price")
 					DetailRow(label: "Volume", value: "\(item.volume.roundTo(places: 2)) gal")
+						.accessibilityIdentifier("volume")
 					DetailRow(label: "Gas mileage", value: "\(item.gasMileage.roundTo(places: 2)) mpg")
+						.accessibilityIdentifier("gas_mileage")
 				}
 			}
 			
 			Section {
 				Toggle(String("Tank filled up?"), isOn: $item.isFilledUp)
+					.accessibilityIdentifier("tank_filled_toggle")
 			}
 		}
+		.accessibilityIdentifier("list_view")
 		.navigationTitle("Entry details") // This approach vs ToolbarItem adds accID to NavigationBar
 		.toolbarTitleDisplayMode(.inline)
 		.toolbar {
