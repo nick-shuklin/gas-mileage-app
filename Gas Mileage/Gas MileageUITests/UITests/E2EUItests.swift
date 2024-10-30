8fa8a13ec71b83021aa1607942076a4857e3575c
Nick Shuklin <nickshuklin@icloud.com>
2024-10-30 16:49:40 -0700

    new e2e test added

--- Gas Mileage/Gas MileageUITests/UITests/E2EUItests.swift
+++ Gas Mileage/Gas MileageUITests/UITests/E2EUItests.swift
@@ -28,9 +28,9 @@ final class E2EUItests: BaseUITest {
 							 price: priceValue,
 							 volume: volumeValue)
 			.tapSaveEntryButton()
-			.verifyAlertIsNOTdisplayed()
+			.verifyAlert(isDisplayed: false)
 		EntriesScreen()
-			.verifyEntry(isDeleted: false,
+			.verifyEntry(isDisplayed: false,
 						 odometerValue: newOdometerValue)
 		SoftAssert.shared.checkForFailures()
 		
@@ -38,5 +38,37 @@ final class E2EUItests: BaseUITest {
 			EntriesScreen()
 				.deleteFirstEntry()
 		}
+	}
+	
+	func testAddEntryWithInvalidData() {
+		var firstEntryOdometerValue = ""
+		let totalValue = "86"
+		let priceValue = "4.56"
+		let volumeValue = "18.86"
+		let mileageFromLastFillUp = 50
+		var newOdometerValue = ""
+		
+		Helpers()
+			.waitForApplicationToLaunch()
+		MainScreen()
+			.tapEntriesTabBarButton()
+		EntriesScreen()
+			.getOdometerFromFirstEntry(&firstEntryOdometerValue)
+			.tapAddEntryButton()
+		EditEntryOverlay()
+			.typeInAllFields(odometer: {
+				newOdometerValue = String((Int(firstEntryOdometerValue) ?? 0) - mileageFromLastFillUp)
+				return newOdometerValue
+			}(),
+							 total: totalValue,
+							 price: priceValue,
+							 volume: volumeValue)
+			.tapSaveEntryButton()
+			.verifyAlert(isDisplayed: true)
+			.tapAlertCancelButton()
+		EntriesScreen()
+			.verifyEntry(isDisplayed: false,
+						 odometerValue: newOdometerValue)
+		SoftAssert.shared.checkForFailures()
 	}
 }
