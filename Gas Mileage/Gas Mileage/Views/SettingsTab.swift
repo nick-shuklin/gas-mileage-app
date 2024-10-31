defa7e507ebdee1c514d407e30da752d1c9721a2
Nick Shuklin <nickshuklin@icloud.com>
2024-10-31 08:43:53 -0700

    removed one section in settings view

--- Gas Mileage/Gas Mileage/Views/SettingsTab.swift
+++ Gas Mileage/Gas Mileage/Views/SettingsTab.swift
@@ -13,18 +13,6 @@ struct SettingsTab: View {
 			
 			Form {
 				Section() {
-					Toggle(isOn: $isMetric) {
-						Label("Imperial or Metric",
-							  systemImage: isMetric ? "m.square.fill" : "i.square.fill")
-					}
-					.toggleStyle(SwitchToggleStyle(tint: .cyan))
-				} header: {
-					Text("Unit settings")
-				} footer: {
-					Text("Default settings applied based on iPhone settings. Metric: km for odometer, liters per 100km for gas mileage. Imperial: ml and miles per gallon (mpg)")
-				}
-				
-				Section() {
 					Toggle(String("Recalculate MPG in ALL entries"), isOn: $inProgress)
 						.toggleStyle(SwitchToggleStyle(tint: .cyan))
 						.onChange(of: inProgress) {
