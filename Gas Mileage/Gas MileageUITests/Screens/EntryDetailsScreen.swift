970aeb160d2f28a5319dcec5052e097c75cce31d
Nick Shuklin <nickshuklin@icloud.com>
2024-10-31 09:42:11 -0700

    comments in entrydetails screen class

--- Gas Mileage/Gas MileageUITests/Screens/EntryDetailsScreen.swift
+++ Gas Mileage/Gas MileageUITests/Screens/EntryDetailsScreen.swift
@@ -2,7 +2,8 @@ import Foundation
 import XCTest
 
 class EntryDetailsScreen: BaseScreen {
-	// MARK: - Static Screen Elements (in order top to the bottom, left to right how they displayed on the screen)
+	// MARK: - Static Screen Elements
+	// Essential elements on the Entry Details screen in order of appearance.
 	private lazy var backButton = app.buttons[LocalizedString.string(forKey: "List of entries")].firstMatch
 	private lazy var navigationBarText = app.staticTexts[LocalizedString.string(forKey: "Entry details")].firstMatch
 	private lazy var editEntryButton = app.buttons[AccIDs.EntriesScreen.editEntryButton.rawValue].firstMatch
@@ -51,8 +52,6 @@ class EntryDetailsScreen: BaseScreen {
 	}
 	private lazy var tankFilledToggle = app.switches[AccIDs.EntryDetailsView.tankFilledToggle.rawValue].firstMatch
 	
-	// MARK: - Dynamic Screen Element
-	
 	// MARK: - Strings
 	private let failureMessageAddOn = "'Entry details screen'"
 	private let odometerTextLabel = LocalizedString.string(forKey: AccIDs.EntryDetailsView.odometer.rawValue)
@@ -61,10 +60,13 @@ class EntryDetailsScreen: BaseScreen {
 	private let volumeTextLabel = LocalizedString.string(forKey: AccIDs.EntryDetailsView.volume.rawValue)
 	private let gasMileageTextLabel = LocalizedString.string(forKey: AccIDs.EntryDetailsView.gasMileage.rawValue)
 	
+	// MARK: - Initialization
+	/// Initializes the screen and verifies that it's loaded.
 	init() {
 		assertScreenIsDisplayed()
 	}
 	
+	/// Checks that the Entry Details screen is displayed by verifying key elements.
 	internal func assertScreenIsDisplayed() {
 		runActivity(.screen, "Then verify \(failureMessageAddOn) is loaded") {
 			XCTAssert(navigationBarText.wait(for: .long),
@@ -98,53 +100,54 @@ class EntryDetailsScreen: BaseScreen {
 	}
 	
 	// MARK: - Assertions
+	/// Verifies that all static elements exist and have correct labels on the Entry Details screen.
 	@discardableResult
 	func verifyAllStaticElements() -> Self {
-		runActivity(.assert, "Then verify all static elements exists and labels match on \(failureMessageAddOn)") {
+		runActivity(.assert, "Then verify all static elements exist and labels match on \(failureMessageAddOn)") {
 			SoftAssert.shared.assert(navigationBarText.wait(),
-									 "'List of entries' text doesn't exists on \(failureMessageAddOn)")
+									 "'List of entries' text doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(backButton.wait(),
-									 "'Back' button doesn't exists on \(failureMessageAddOn)")
+									 "'Back' button doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(editEntryButton.wait(),
-									 "'Edit entry' button doesn't exists on \(failureMessageAddOn)")
+									 "'Edit entry' button doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(listView.wait(),
-									 "'Collection' view doesn't exists on \(failureMessageAddOn)")
+									 "'Collection' view doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(fillupDateText.wait(),
-									 "'Fill up date' text doesn't exists on \(failureMessageAddOn)")
+									 "'Fill up date' text doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(gasStationNameText.wait(),
-									 "Gas station name text doesn't exists on \(failureMessageAddOn)")
+									 "Gas station name text doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(((odometerText?.wait()) != nil),
-									 "'Odometer' text doesn't exists on \(failureMessageAddOn)")
+									 "'Odometer' text doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assertEqual(odometerText?.label, odometerTextLabel,
-					  "label does NOT match on \(failureMessageAddOn)")
+										  "Label does NOT match on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(((odometerData?.wait()) != nil),
-									 "'Odometer' data doesn't exists on \(failureMessageAddOn)")
+									 "'Odometer' data doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(((totalText?.wait()) != nil),
-									 "'Total' text doesn't exists on \(failureMessageAddOn)")
+									 "'Total' text doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assertEqual(totalText?.label, totalTextLabel,
-					  "label does NOT match on \(failureMessageAddOn)")
+										  "Label does NOT match on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(((totalData?.wait()) != nil),
-									 "'Total' data doesn't exists on \(failureMessageAddOn)")
+									 "'Total' data doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(((priceText?.wait()) != nil),
-									 "'Price' text doesn't exists on \(failureMessageAddOn)")
+									 "'Price' text doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assertEqual(priceText?.label, priceTextLabel,
-					  "label does NOT match on \(failureMessageAddOn)")
+										  "Label does NOT match on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(((priceData?.wait()) != nil),
-									 "'Price' data doesn't exists on \(failureMessageAddOn)")
+									 "'Price' data doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(((volumeText?.wait()) != nil),
-									 "'Volume' text doesn't exists on \(failureMessageAddOn)")
+									 "'Volume' text doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assertEqual(volumeText?.label, volumeTextLabel,
-					  "label does NOT match on \(failureMessageAddOn)")
+										  "Label does NOT match on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(((volumeData?.wait()) != nil),
-									 "'Volume' data doesn't exists on \(failureMessageAddOn)")
+									 "'Volume' data doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(((gasMileageText?.wait()) != nil),
-									 "'Gas mileage' text doesn't exists on \(failureMessageAddOn)")
+									 "'Gas mileage' text doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assertEqual(gasMileageText?.label, gasMileageTextLabel,
-					  "label does NOT match on \(failureMessageAddOn)")
+										  "Label does NOT match on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(((gasMileageData?.wait()) != nil),
-									 "'Gas mileage' data doesn't exists on \(failureMessageAddOn)")
+									 "'Gas mileage' data doesn't exist on \(failureMessageAddOn)")
 			SoftAssert.shared.assert(tankFilledToggle.wait(),
-									 "'Tank filled up?' toggle doesn't exists on \(failureMessageAddOn)")
+									 "'Tank filled up?' toggle doesn't exist on \(failureMessageAddOn)")
 		}
 		return self
 	}
